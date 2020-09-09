848a6951029e5880251b9da280d1c51d5dff2210
Saeed <abdulrahmansaeed57@gmail.com>
2020-09-09 20:50:54 +0200

    Added entity to movie class

--- app/src/main/java/com/ammar/teamandroidaholicsmovieapp/data/vo/Movie.kt
+++ app/src/main/java/com/ammar/teamandroidaholicsmovieapp/data/vo/Movie.kt
@@ -1,9 +1,12 @@
 package com.ammar.teamandroidaholicsmovieapp.data.vo
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
-
+@Entity(tableName="movies_table")
 data class Movie(
+    @PrimaryKey
     val id: Int,
     @SerializedName("poster_path")
     val posterPath: String,
